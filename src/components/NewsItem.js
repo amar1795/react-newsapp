d56ffac419fefd1daa3df6c9dbad169eef20726f
Amar <jeetamar496@gmail.com>
2023-06-16 01:53:11 +0530

    constructor created and json added to articles

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
-      let {title,description}=this.props;
+      let {title,description,ImageUrl}=this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://sportshub.cbsistatic.com/i/r/2023/06/15/cbc4c602-2055-4994-9591-73f388ad5a18/thumbnail/1200x675/10b5ed7c77bc50e67db8f8888f163c43/us-open-hill-sign-p3-g.png" className="card-img-top" alt="..."/>
+        <img src={ImageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
