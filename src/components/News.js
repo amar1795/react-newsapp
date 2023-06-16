a26cac73e0762cd0ed5eaae7b558c59b5495a70b
Amar <jeetamar496@gmail.com>
2023-06-16 20:39:30 +0530

    loading effect added and variable page size added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from "./Spinner"
 
 export class News extends Component {
     articles=[
@@ -11,16 +12,19 @@ export class News extends Component {
         this.state={
             articles:this.articles,
             loading:false,
-            page:1
+            page:1 //current state
+
         }
         
     }
 
     async componentDidMount(){
-      let url="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=1&pagesize=20"
+      let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=1&pagesize=${this.props.pagesize}`
+      this.setState({loading:true})
       let data=await fetch(url);
       let parsedData=await data.json();
       console.log(parsedData)
+      this.setState({loading:false})
       this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
     }
 
@@ -28,9 +32,11 @@ export class News extends Component {
     handlePreviousClick =async()=>{
       
         console.log("Previous click");
-        let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=${this.state.page-1}&pagesize=20`;
+        let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=${this.state.page-1}&pagesize=${this.props.pagesize}`;
+        this.setState({loading:true})
         let data=await fetch(url);
         let parsedData=await data.json();
+        this.setState({loading:false})
         this.setState({
           articles:parsedData.articles,
           page:this.state.page-1
@@ -46,7 +52,7 @@ export class News extends Component {
       else
       {
       console.log("Previous click");
-      let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=${this.state.page+1}&pagesize=20`;
+      let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4136a686f9784b0cae639181c30d9814&page=${this.state.page+1}&pagesize=${this.props.pagesize}`;
       let data=await fetch(url);
       let parsedData=await data.json();
       this.setState({
@@ -62,23 +68,26 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3' >
-    <h2>NewsMonkey - top Headlines</h2>   
+    <h1 className='text-center my-4'>NewsMonkey - TOP HEADLINES</h1> 
+    <div className="text-center my-4">
+    {this.state.loading && <Spinner/>}
+    </div>  
     <div className='row'>
-      {this.state.articles.map((element)=>{
-       return  <div className='col-md-4' key={element.url}>
+      {!this.state.loading && this.state.articles.map((element)=>{
+       return  <div className='col-md-4 my-3' key={element.url}>
         <NewsItem title={element.title.slice(0,44)} description={element.description?.slice(0,88)} newsUrl={element.url} ImageUrl={element.urlToImage}/> 
         </div>
       })}
        </div>   
        <div className="container d-flex justify-content-between my-3 ">
        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
-        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        <button disabled={this.state.page +1>Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>  
 
     </div>
     
     )
-  }Name
+  }
 }
 
 export default News
